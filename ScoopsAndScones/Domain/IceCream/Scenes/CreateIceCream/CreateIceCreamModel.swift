4996d9df0d90d40c2a4e662784913d7e3169359a
noseeevil <oleksandrpatsora@gmail.com>
2022-04-10 19:59:18 +0300

    creating a model

---
+++ ScoopsAndScones/Domain/IceCream/Scenes/CreateIceCream/CreateIceCreamModel.swift
@@ -0,0 +1,15 @@
+enum CreateIceCream {
+  enum LoadIceCream {
+    struct Request {}
+
+    struct Response {
+      var iceCreamData: IceCream
+    }
+
+    struct ViewModel {
+      var cones: [String]
+      var flavors: [String]
+      var toppings: [String]
+    }
+  }
+}
